7baff5a0358fe0453173f1231d2dd03286560f47
Harsha Vardhan <140612174+Harsha707@users.noreply.github.com>
2024-09-22 09:06:12 +0530

    Update index.ts

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 import { authRouter } from "./auth-router";
 import { publicProcedure, router } from "./trpc";
-import { QueryValidator } from "../lib/validators/query-validator";
+import { QueryValidator } from "@/lib/validators/quary-validator";
 import { getPayloadClient } from "../get-payload";
 import { paymentRouter } from "./payment-router";
 
